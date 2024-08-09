fb32a3201dfc2d35e0b37e2114b99adfcf413c3f
Kawaryo <khrk7672@gmail.com>
2024-08-10 01:59:23 +0900

    Update

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+import { basePath } from "../../next.config";
+const BASE_PATH = basePath ? basePath : "";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      
+    </main>
+  );
+}
