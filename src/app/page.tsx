5e4b57950e0ab98cc12cc88f48deea9575d0ac43
Kawaryo <khrk7672@gmail.com>
2024-08-13 18:50:59 +0900

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@ import Collapsible from "./components/collapsible";
 
 export default function Home() {
   return (
-    <main>
-      <h1>開成学園天文気象部 音声読み上げ機能</h1>
-      <Collapsible header="説明1" file="1.mp3"/>
-      <Collapsible header="説明2" file="2.mp3"/>
-    </main>
+    <>
+      <link rel="icon" href="logo.svg" type="image/x-icon"></link>
+      <main>
+        <h1>開成学園天文気象部 音声読み上げ機能</h1>
+        <Collapsible header="説明1" file="1.mp3"/>
+        <Collapsible header="説明2" file="2.mp3"/>
+      </main>
+    </>
+    
   );
 }
