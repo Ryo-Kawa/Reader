a8bb6d2f34ec69a7ac2ba215f0d055fbd6711ad8
Asa-kawa <kh7672ak@gmail.com>
2024-09-10 18:37:05 +0900

    a

--- src/components/main/page/welcome/welcome.tsx
+++ src/components/main/page/welcome/welcome.tsx
@@ -4,8 +4,7 @@ import { BASE_PATH } from "@/components/home";
 export default function Welcome() {
     return (
         <section>
-            <Image src={`${BASE_PATH}./image/welcome.svg`} alt="天文気象部へようこそ！" width={200} height={150}></Image>
-            <button onClick={() => alert(BASE_PATH)}>Click Me!</button>
+            <Image src={`${BASE_PATH}/image/welcome.svg`} alt="天文気象部へようこそ！" width={200} height={150}></Image>
         </section>
     );
 }
