655f41562893e77a363d0a4e8bc11d6680152149
Ryo-Kawa <152622724+Ryo-Kawa@users.noreply.github.com>
2024-09-20 10:12:52 +0900

    Update page.tsx

--- src/app/display/page.tsx
+++ src/app/display/page.tsx
@@ -2,8 +2,17 @@
 
 import Display from "@/component/main/page/component/display/display";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from 'react';
 
 export default function Home() {
+    return (
+        <Suspense>
+            <HomePage />
+        </Suspense>
+    );
+}
+
+function HomePage() {
     const searchParams = useSearchParams();
 
     const index = Number(searchParams.get("index"));
