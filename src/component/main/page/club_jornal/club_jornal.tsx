1c3105607b680dc87051569f5058883e110dd581
Ryo-Kawa <152622724+Ryo-Kawa@users.noreply.github.com>
2024-09-20 10:41:15 +0900

    Create club_jornal.tsx

---
+++ src/component/main/page/club_jornal/club_jornal.tsx
@@ -0,0 +1,8 @@
+export default function ClubJornal() {
+    return (
+        <section>
+            <Header title="部誌" />
+            <p>オンライン部誌は<a href="https://drive.google.com/file/d/1SZyjIK-c53WSN8EaBqHOhJbgEJKxxX1g/view?usp=drivesdk">こちら</a>から</p>
+        </section>
+    );
+}
