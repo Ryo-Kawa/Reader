0b827c310fff467e7c32a0a09defa463ffaa7325
Asa-kawa <kh7672ak@gmail.com>
2024-09-20 23:55:22 +0900

    a

--- src/component/display/footer/footer.tsx
+++ src/component/display/footer/footer.tsx
@@ -7,7 +7,7 @@ import { display_num } from "@/component/all_exports";
 export default function Footer({ index }: { index: number }) {
     return (
         <section className={styles.footer}>
-            <nav>
+            <nav className={styles.nav}>
                 <Link href={`/display?index=${index - 1}`}>{0 < index ? "前" : ""}</Link>
                 <Link href="/"><FontAwesomeIcon icon={faHouse} /></Link>
                 <Link href={`/display?index=${index + 1}`}>{index + 1 < display_num ? "後" : ""}</Link>
