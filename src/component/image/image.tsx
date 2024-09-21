eba44d02292c8bc87c79a5d56dba5ab6cb03ee37
Ryo-Kawa <152622724+Ryo-Kawa@users.noreply.github.com>
2024-09-22 07:06:27 +0900

    Update image.tsx

--- src/component/image/image.tsx
+++ src/component/image/image.tsx
@@ -4,7 +4,7 @@ import { ReactElement } from "react";
 export default function _Image({ path, title } : { path: string, title: string | ReactElement }) {
   return (
     <div className={styles.imageWrapper}>
-      <img src={path} className={styles.image} />
+      <img src={path} className={styles.image}/>
       <p>{title}</p>
     </div>
   );
