29ec13666ad419aecf62bf5ad0ac7c4d700180ef
Ryo-Kawa <152622724+Ryo-Kawa@users.noreply.github.com>
2024-09-21 10:40:50 +0900

    Update video.tsx

--- src/component/video/video.tsx
+++ src/component/video/video.tsx
@@ -1,6 +1,7 @@
+import ReactElement from "react";
 import styles from "./video.module.css";
 
-export default function Video({ path, title }: { path: string, title: string }) {
+export default function Video({ path, title }: { path: string, title: string | ReactElement }) {
     return (
         <div className={styles.videoWrapper}>
             <p>{title}</p>
