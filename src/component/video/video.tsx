2499a78ae38c4ce87d486fc4c14a356a81144206
Ryo-Kawa <152622724+Ryo-Kawa@users.noreply.github.com>
2024-09-21 10:56:35 +0900

    Update video.tsx

--- src/component/video/video.tsx
+++ src/component/video/video.tsx
@@ -1,4 +1,4 @@
-import ReactElement from "react";
+import { ReactElement } from "react";
 import styles from "./video.module.css";
 
 export default function Video({ path, title }: { path: string, title: string | ReactElement }) {
