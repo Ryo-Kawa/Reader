691c795010555fa5b6b9e4eab2eaeab7fe5f17b3
Ryo-Kawa <152622724+Ryo-Kawa@users.noreply.github.com>
2024-09-22 09:09:23 +0900

    Update sandan_grand_prix.tsx

--- src/component/home/main/sandan_grand_prix/sandan_grand_prix.tsx
+++ src/component/home/main/sandan_grand_prix/sandan_grand_prix.tsx
@@ -6,7 +6,7 @@ export default function SandanGrandPrix() {
         <section>
             <h1 className={main_styles.header}>参団グランプリ</h1>
             <div className={main_styles.content}>
-                <p>投票してくれると幸いです。<br />投票場所は２つ下のB<Ruby text="棟" ruby="とう" />１階にあります。</p>
+                <p>投票してくれると幸いです。<br />投票場所は２つ下のB<Ruby text="棟" ruby="とう" />１階にあります。<br />(※投票は13時までです。)</p>
             </div>
         </section>
     );
